54d22f76783b6d92e6058e80d41153c0d9438aba
shyamshyam018 <131571361+shyamshyam018@users.noreply.github.com>
2023-04-29 07:14:35 +0530

    Update predictive_system.py

--- predictive_system.py
+++ predictive_system.py
@@ -9,7 +9,7 @@ import numpy as np
 import pickle
 
 # loading the saved model
-loaded_model = pickle.load(open('C:/Users/sss/Downloads/trained_model.sav', 'rb'))
+loaded_model = pickle.load(open('trained_model.sav', 'rb'))
 
 input_data = (5,166,72,19,175,25.8,0.587,51)
 
