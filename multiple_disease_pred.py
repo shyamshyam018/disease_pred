fc47b1b800ad2bacf34186463c4277ef9ff18531
shyamshyam018 <131571361+shyamshyam018@users.noreply.github.com>
2023-05-16 14:56:57 +0530

    Update multiple_disease_pred.py

--- multiple_disease_pred.py
+++ multiple_disease_pred.py
@@ -7,11 +7,11 @@ from streamlit_option_menu import option_menu
 
 # loading the saved models
 
-diabetes_model = pickle.load(open('C:/Users/sss/OneDrive/Desktop/MACHINE LEARNING/MULTIPLE DISEASE PREDICTION SYSTEM/trained_model.sav', 'rb'))
+diabetes_model = pickle.load(open('trained_model.sav', 'rb'))
 
-heart_disease_model = pickle.load(open('C:/Users/sss/OneDrive/Desktop/MACHINE LEARNING/MULTIPLE DISEASE PREDICTION SYSTEM/heart_disease_model.sav','rb'))
+heart_disease_model = pickle.load(open('heart_disease_model.sav','rb'))
 
-parkinsons_model = pickle.load(open('C:/Users/sss/OneDrive/Desktop/MACHINE LEARNING/MULTIPLE DISEASE PREDICTION SYSTEM/parkinsons_model (1).sav', 'rb'))
+parkinsons_model = pickle.load(open('parkinsons_model (1).sav', 'rb'))
 
 
 
